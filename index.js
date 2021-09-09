0f30eabf1a058f8da5438c0440920457a012d865
danilobatson <djbatson19@gmail.com>
2021-09-09 12:24:38 -0700

    added table

--- index.js
+++ index.js
@@ -3,20 +3,32 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+
 app.set("view engine", "pug");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+
 
 const users = [
   {
     id: 1,
-    firstName: "Jill",
-    lastName: "Jack",
-    email: "jill.jack@gmail.com"
-  }
+    firstName: 'Jill',
+    lastName: 'Jack',
+    email: 'jill.jack@gmail.com',
+  },
 ];
+app.get("/", async (req, res) => {
+  //res.send("Hello World!");
+  res.render('index', {users})
+});
+
+// const users = [
+//   {
+//     id: 1,
+//     firstName: "Jill",
+//     lastName: "Jack",
+//     email: "jill.jack@gmail.com"
+//   }
+// ];
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
